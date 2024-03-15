75edb0cb392e35d74281fd7c386e2a7956ddcf2d
salmaarayyan <salmaarayyan@gmail.com>
2024-03-15 14:59:11 +0700

    InsertionSort

--- 082_InsertionSort/082_InsertionSort.cpp
+++ 082_InsertionSort/082_InsertionSort.cpp
@@ -1,20 +1,74 @@
-// 082_InsertionSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+int arr[12];
+int n;
+
+void input() { //prosedur untuk input
+
+    while (true) {
+        cout << "Masukan banyaknya elemen pada array "; // Output ke layar
+        cin >> n;    // input dari pengguna
+        if (n <= 12) // jika n kurang dari atau sama dengan 12
+            break;   // keluar dari loop
+        else {
+            cout << "\nArray dapat mempunyai maksimal 12 elemen.\n"; // output ke layar
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < n; i++) {            //looping dengan i dimulai dari 0 hingga n-1 
+        cout << "Data ke-" << (i + 1) << ":";// output ke layar
+        cin >> arr[i];                       // memasukan output dari pengguna
+    }
+}
+
+
+void insertionsort() {
+    //deklarasi variabel
+    int i, temp, j;
+    for (i = 1; i <= n - 1; i++) {//step 1
+        temp = arr[i];//step 2
+        j = i - 1;// step 3
+        while (j >= 0 && arr[j] > temp)//step 4
+        {
+            arr[j + 1] = arr[j];//step 4a
+            j = j - 1;//step 4b
+        }
+        arr[j + 1] = temp;//step 5
+    }
+}
+
+void display() {
+    cout << endl;
+    cout << "================================" << endl;
+    cout << "Elemen Array yang telah tersusun" << endl;
+    cout << "================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j]; //output setiap element array pada garis baru
+        if (j < n - 1) {
+            cout << "-->";
+        }
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+
+}
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    input();
+    insertionsort();
+    display();
+    system("pause");
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
